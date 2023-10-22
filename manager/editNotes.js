8fc2036c463543fff9631f8e64357eb74af46671
S0l3b0 <142552926+S0l3b0@users.noreply.github.com>
2023-10-22 15:50:31 -0700

    issue 2

--- manager/editNotes.js
+++ manager/editNotes.js
@@ -9,15 +9,15 @@ class editNotes extends Manager {
         super()
     }
 
-    async runTask(uuid, newNote) {
+    async runTask(scouter_uuid, newNote, tournament_key, match_key, team_number) {
 
         var sql = `UPDATE data
         SET notes = ?
-        WHERE uuid = ?`
+        WHERE scouter_uuid = ?`
         
 
         return new Promise(async (resolve, reject) => {
-            Manager.db.all(sql, [newNote, uuid], (err, rows) => {
+            Manager.db.all(sql, [newNote, scouter_uuid, tournament_key, match_key, team_number], (err, rows) => {
                 if(err)
                 {
                     console.log(err)
